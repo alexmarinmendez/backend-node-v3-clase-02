e6677b276889f30f6b62a3a7edf993ddd788c81f
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-01-09 05:40:00 -0500

    feat: add a let example

---
+++ demo-02-let.js
@@ -0,0 +1,37 @@
+// let i = 0;
+
+// function myRun() {
+//     i = 1;
+//     let j = 2;
+
+//     if (true) {
+//         console.log(i);
+//         console.log(j);
+//     }
+// }
+
+// myRun();
+
+
+
+// function myRun() {
+//     let i = 0;
+
+//     if(true) {
+//         let i = 1;
+//         console.log(i);
+//     }
+//     console.log(i);
+// }
+
+// myRun();
+
+
+function myRun() {
+    if(true) {
+        let i = 1;
+    }
+    console.log(i)
+}
+
+myRun();
